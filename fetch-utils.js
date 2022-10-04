08785d6ac11c90c4cd6f567e4df0a061e74bd300
King Kong <king-kong@gmail.com>
2022-10-04 10:36:15 -0700

    Planning artifact (pages, wire frames, state and events) for app

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://lotqrshldcgiswwaymns.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvdHFyc2hsZGNnaXN3d2F5bW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4MzkyOTcsImV4cCI6MTk4MDQxNTI5N30.akWz2i6t3m1urnZLvwFf_Rapxuou6DpmEXQQf79bS_c';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
